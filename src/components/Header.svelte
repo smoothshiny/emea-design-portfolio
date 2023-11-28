140822dcf339fb7c973ad4ea645a3eba39e73081
smoothshiny <clay@pixelfuel.com>
2023-11-27 22:14:24 -0500

    add burger menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,20 @@
 <script>
   export let y;
-
-  let tabs = [
-    { name: "About me", link: "#about" },
-    { name: "Portfolio", link: "#portfolio" },
-    { name: "Contact", link: "#contact" },
-  ];
+  let burger = 1;
+  function clickBurger() {
+    let list = document.querySelector("ul");
+    burger === 1
+      ? ((burger = 2),
+        list.classList.add("top-[80px]"),
+        list.classList.add("opacity-100"),
+        list.classList.add("bg-white"),
+        list.classList.add("text-right"))
+      : ((burger = 1),
+        list.classList.remove("top-[80px]"),
+        list.classList.remove("opacity-100"),
+        list.classList.remove("bg-white"),
+        list.classList.remove("text-right"));
+  }
 </script>
 
 <header
@@ -16,16 +25,25 @@
 >
   <img src={"images/eadesign.png"} alt="EA Design" class="max-h-[70px]" />
 
-  <div class="sm:flex items-center gap-4 hidden">
-    {#each tabs as tab, index}
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <a
-        href={tab.link}
-        class="duration-200 hover:text-emerald-800"
-        target={index === 2 ? "_blank" : ""}
+  <span class="text-3xl cursor-pointer mx-2 md:hidden block">
+    <button name="menu" on:click={clickBurger}>&#9776;</button>
+  </span>
+
+  <ul
+    class="md:bg-cyan-50 md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px]"
+  >
+    <li class="mx-4 my-6 md:my-0">
+      <a href="#about" class="duration-200 hover:text-emerald-800">About me</a>
+    </li>
+    <li class="mx-4 my-6 md:my-0">
+      <a href="#portfolio" class="duration-200 hover:text-emerald-800"
+        >Portfolio</a
       >
-        <p>{tab.name}</p>
-      </a>
-    {/each}
-  </div>
+    </li>
+    <li class="mx-4 my-6 md:my-0">
+      <a href="#contact" class="duration-200 hover:text-emerald-800"
+        >Contact me</a
+      >
+    </li>
+  </ul>
 </header>
