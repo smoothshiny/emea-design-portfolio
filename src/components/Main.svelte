1b021fe0b931741910c36c65c021f1103e4d2f62
smoothshiny <clay@pixelfuel.com>
2023-11-29 21:38:55 -0500

    center  welcome

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -21,21 +21,18 @@
 
 <main class="flex flex-col flex-1 p-4">
   <section id="intro" class="grid grid-cols-1 sm:py-14">
-    <div class="flex flex-col lg:text-left m-20 gap-6 md:gap-8 lg:gap-10">
+    <div
+      class="flex flex-col place-items-center lg:text-left m-20 gap-6 md:gap-8 lg:gap-10"
+    >
       <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Welcome to EA design!
       </h2>
-      <p class="text-base sm:text-lg md:text-xl">
-        My goal as a graphic designer is to create visually appealing products
-        and build professional connections with my clients.
-      </p>
 
       <button
-        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-emerald-800 text-cyan-50"
+        class="blueShadow place-items-center lg:mr-auto lg:text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-emerald-800 text-cyan-50"
         ><h4 class="relative z-9">Contact me &rarr;</h4>
       </button>
     </div>
-    <div class="relative shadow-2xl grid place-items-center" />
   </section>
 
   <section
