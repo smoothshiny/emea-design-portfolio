c81a26d525be9f5ccb12753dd60ffe110ddc0cf0
smoothshiny <clay@pixelfuel.com>
2023-12-02 16:29:24 -0500

    add footer stuff

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,7 @@
-<div class="text-center text-md mb-2">216.272.2587 | ellaatfield@gmail.com</div>
+<div class="text-center text-md mb-2">
+  <img
+    src={"images/instaicon.png"}
+    alt="Instagram"
+    class="max-h-[20px] m-2 inline"
+  />| 216.272.2587 | ellaatfield@gmail.com
+</div>
